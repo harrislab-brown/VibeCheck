173475693aae946e573f6a0b2d7cfc16a5e102b7
johntantolik <john_antolik@brown.edu>
2024-10-01 14:56:31 -0400

    started work on the accelerometer implementation

---
+++ Firmware/VibeCheck/Core/Inc/LSM6DS3.h
@@ -0,0 +1,121 @@
+/*
+ * LSM6DS3.h
+ *
+ *  Created on: Jul 16, 2024
+ *      Author: johnt
+ */
+
+#ifndef INC_LSM6DS3_H_
+#define INC_LSM6DS3_H_
+
+
+#include "main.h"
+
+
+/* device defines */
+#define LSM6DS3_DEVICE_ID 			0b01101010 /* fixed value of WHO_AM_I register */
+#define LSM6DS3_RESOLUTION 			16  /* bit depth of sensor */
+
+/* device register addresses (p.49) */
+#define LSM6DS3_REG_INT1_CTRL		0x0D
+#define LSM6DS3_REG_INT2_CTRL 		0x0E
+
+#define LSM6DS3_REG_WHO_AM_I 		0x0F
+
+#define LSM6DS3_REG_CTRL1_XL  		0x10
+#define LSM6DS3_REG_CTRL2_G  		0x11
+#define LSM6DS3_REG_CTRL3_G  		0x12
+#define LSM6DS3_REG_CTRL4_C  		0x13
+#define LSM6DS3_REG_CTRL5_C  		0x14
+#define LSM6DS3_REG_CTRL6_C 		0x15
+#define LSM6DS3_REG_CTRL7_G 		0x16
+#define LSM6DS3_REG_CTRL8_XL        0x17
+#define LSM6DS3_REG_CTRL9_XL        0x18
+#define LSM6DS3_REG_CTRL10_C        0x19
+
+#define LSM6DS3_REG_STATUS_REG      0x1E
+
+#define LSM6DS3_REG_OUT_TEMP_L 		0x20  /* temperature sensor data */
+#define LSM6DS3_REG_OUT_TEMP_H 		0x21
+
+#define LSM6DS3_REG_OUTX_L_G 		0x22  /* gyroscope output registers */
+#define LSM6DS3_REG_OUTX_H_G 		0x23
+#define LSM6DS3_REG_OUTY_L_G 		0x24
+#define LSM6DS3_REG_OUTY_H_G 		0x25
+#define LSM6DS3_REG_OUTZ_L_G 		0x26
+#define LSM6DS3_REG_OUTZ_H_G 		0x27
+
+#define LSM6DS3_REG_OUTX_L_XL 		0x28  /* accelerometer output registers */
+#define LSM6DS3_REG_OUTX_H_XL 		0x29
+#define LSM6DS3_REG_OUTY_L_XL 		0x2A
+#define LSM6DS3_REG_OUTY_H_XL 		0x2B
+#define LSM6DS3_REG_OUTZ_L_XL 		0x2C
+#define LSM6DS3_REG_OUTZ_H_XL 		0x2D
+
+#define LSM6DS3_REG_X_OFS_USR 		0x73  /* user offsets */
+#define LSM6DS3_REG_Y_OFS_USR 		0x74
+#define LSM6DS3_REG_Z_OFS_USR 		0x75
+
+/* register values */
+#define LSM6DS3_ODR_DISABLE 		0b00000000  /* CTRL1_XL register */
+
+
+#define LSM6DS3_G_RANGE_2  			0b00000000  /* CTRL1_XL register */
+#define LSM6DS3_G_RANGE_4  			0b00001000
+#define LSM6DS3_G_RANGE_8  			0b00001100
+#define LSM6DS3_G_RANGE_16 			0b00000100
+
+typedef struct
+{
+	/* TODO: add user offsets and LPF settings */
+
+	uint32_t accel_odr_hz;  /* 13, 26, 52, 104, 208, 416, 833, 1660, 3330, 6660 */
+	uint32_t g_range;  /* 2, 4, 8, or 16 */
+
+	uint32_t gyro_odr_hz;  /* 13, 26, 52, 104, 208, 416, 833, 1660, 3330, 6660 */
+	uint32_t dps_range;  /* 245, 500, 1000, 2000 */
+
+} LSM6DS3_Config;
+
+typedef struct
+{
+	/* SPI handle */
+	SPI_HandleTypeDef* spi;
+
+	/* chip select pin */
+	GPIO_TypeDef* cs_port;
+	uint16_t cs_pin;
+
+	/* interrupt pins */
+	GPIO_TypeDef* int1_port;
+	uint16_t int1_pin;
+	GPIO_TypeDef* int2_port;
+	uint16_t int2_pin;
+
+	/* pointer to object that stores configuration parameters */
+	LSM6DS3_Config* config;
+
+} LSM6DS3;
+
+uint32_t LSM6DS3_Init(LSM6DS3* sensor, LSM6DS3_Config* config, SPI_HandleTypeDef* spi,
+		GPIO_TypeDef* cs_port, uint16_t cs_pin,
+		GPIO_TypeDef* int1_port, uint16_t int1_pin,
+		GPIO_TypeDef* int2_port, uint16_t int2_pin);
+
+uint32_t LSM6DS3_TestCommunication(LSM6DS3* sensor);
+void LSM6DS3_Configure(LSM6DS3* sensor);
+
+void LSM6DS3_Enable(LSM6DS3* sensor);
+void LSM6DS3_Disable(LSM6DS3* sensor);
+
+void LSM6DS3_ReadAcceleration(LSM6DS3* sensor, float* x, float* y, float * z);  /* get the acceleration in g */
+void LSM6DS3_ReadGyro(LSM6DS3* sensor, float* x, float* y, float * z);  /* get the rotation rate in degrees per second */
+
+HAL_StatusTypeDef LSM6DS3_ReadRegister(LSM6DS3* sensor, uint8_t reg, uint8_t* data);
+HAL_StatusTypeDef LSM6DS3_WriteRegister(LSM6DS3* sensor, uint8_t reg, uint8_t data);
+
+uint32_t LSM6DS3_GetInt1State(LSM6DS3* sensor);  /* read the state of the chip interrupt pins */
+uint32_t LSM6DS3_GetInt2State(LSM6DS3* sensor);
+
+
+#endif /* INC_LSM6DS3_H_ */
---
+++ Firmware/VibeCheck/Core/Src/LSM6DS3.c
@@ -0,0 +1,167 @@
+/*
+ * LSM6DS3.c
+ *
+ *  Created on: Jul 16, 2024
+ *      Author: johnt
+ */
+
+#include "LSM6DS3.h"
+#include "fast_spi.h"
+
+
+uint32_t LSM6DS3_Init(LSM6DS3* sensor, LSM6DS3_Config* config, SPI_HandleTypeDef* spi,  /* set the configuration parameters that need to be set once */
+		GPIO_TypeDef* cs_port, uint16_t cs_pin,
+		GPIO_TypeDef* int1_port, uint16_t int1_pin,
+		GPIO_TypeDef* int2_port, uint16_t int2_pin)
+{
+	sensor->config = config;
+	sensor->spi = spi;
+	sensor->cs_port = cs_port;
+	sensor->cs_pin = cs_pin;
+	sensor->int1_port = int1_port;
+	sensor->int1_pin = int1_pin;
+	sensor->int2_port = int2_port;
+	sensor->int2_pin = int2_pin;
+
+	/* verify that we can communicate with the sensor */
+	if (!LSM6DS3_TestCommunication(sensor))
+		return 0;
+
+	/* power down the device and set register values that will not change with the configuration */
+	LSM6DS3_Disable(sensor);
+	(void)LSM6DS3_WriteRegister(sensor, LSM6DS3_REG_INT1_CTRL, 0x01);  /* INT1 set when accel data ready (p. 59) */
+	(void)LSM6DS3_WriteRegister(sensor, LSM6DS3_REG_INT2_CTRL, 0x02);  /* INT2 set when gyro data ready (p. 60) */
+	(void)LSM6DS3_WriteRegister(sensor, LSM6DS3_REG_CTRL4_C, 0b00000100);  /* disable the I2C interface, also disables the gyro LPF1 (p. 64)  TODO: handle the gyro LPF */
+	(void)LSM6DS3_WriteRegister(sensor, LSM6DS3_REG_CTRL6_C, 0b00000000);  /* sets the user offset weights and the gyro LPF bandwidth (p. 66)  TODO: handle the gyro LPF */
+	(void)LSM6DS3_WriteRegister(sensor, LSM6DS3_REG_CTRL7_G, 0b00000000);  /* handles gyro HPF (p. 67)  TODO: implement this in the configuration */
+	(void)LSM6DS3_WriteRegister(sensor, LSM6DS3_REG_CTRL8_XL, 0b00000000);  /* acceleration filters (p. 67)  TODO: implement this in the configuration */
+
+	/* set the register values that depend on the configuration */
+	LSM6DS3_Configure(sensor);
+	return 1;
+}
+
+
+void LSM6DS3_Configure(LSM6DS3* sensor)  /* set register values based on the configuration object */
+{
+	/* TODO: write the user offsets */
+
+	/* enable the sensor with the configured range (p. 32) */
+//	uint8_t range_data;
+//	switch(sensor->config->g_range)
+//	{
+//	case 2:
+//		range_data = LSM6DS3_G_RANGE_2;
+//		break;
+//	case 4:
+//		range_data = LSM6DS3_G_RANGE_4;
+//		break;
+//	case 8:
+//		range_data = LSM6DS3_G_RANGE_8;
+//		break;
+//	case 16:
+//		range_data = LSM6DS3_G_RANGE_16;
+//		break;
+//	default:
+//		range_data = LSM6DS3_G_RANGE_2;
+//		break;
+//	}
+//
+//	reg_data = (LSM6DS3_ODR_26667HZ | range_data);
+//	status = LSM6DS3_WriteRegister(sensor, LSM6DS3_REG_CTRL1_XL, &reg_data);
+//	err_num += (status != HAL_OK);
+//
+//	return err_num;
+}
+
+
+uint32_t LSM6DS3_TestCommunication(LSM6DS3* sensor)  /* check that the sensor is connected by querying its device ID */
+{
+	uint8_t reg_data;
+	HAL_StatusTypeDef status = LSM6DS3_ReadRegister(sensor, LSM6DS3_REG_WHO_AM_I, &reg_data);
+	if (status == HAL_OK && reg_data == LSM6DS3_DEVICE_ID)
+		return 1;
+	return 0;
+}
+
+
+void LSM6DS3_Enable(LSM6DS3* sensor)  /* start gathering data */
+{
+
+}
+
+
+void LSM6DS3_Disable(LSM6DS3* sensor)
+{
+	(void)LSM6DS3_WriteRegister(sensor, LSM6DS3_REG_CTRL1_XL, LSM6DS3_ODR_DISABLE);  /* power down accel. (p. 61) */
+	(void)LSM6DS3_WriteRegister(sensor, LSM6DS3_REG_CTRL2_G, LSM6DS3_ODR_DISABLE);  /* power down gyro. (p. 62) */
+}
+
+
+void LSM6DS3_ReadAcceleration(LSM6DS3* sensor, float* x, float* y, float * z)
+{
+	/* get the acceleration in g */
+
+	/* read multiple bytes corresponding to the raw accelerometer data */
+	uint8_t tx_buf[7] = {(LSM6DS3_REG_OUTX_L_XL | 0x80), 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+	uint8_t rx_buf[7];
+
+	/* use our fast SPI implementation instead of the HAL call */
+	SPI_TxRx_Fast(tx_buf, rx_buf, 7, sensor->spi->Instance, sensor->cs_port, sensor->cs_pin);
+
+	/* convert the raw readings to physical units */
+	int16_t raw_data_x = ((int16_t)(rx_buf[1])) | (((int16_t)(rx_buf[2])) << 8);
+	int16_t raw_data_y = ((int16_t)(rx_buf[3])) | (((int16_t)(rx_buf[4])) << 8);
+	int16_t raw_data_z = ((int16_t)(rx_buf[5])) | (((int16_t)(rx_buf[6])) << 8);
+
+	*x = sensor->config->g_range * (float)raw_data_x / (float)(1 << (LSM6DS3_RESOLUTION - 1));
+	*y = sensor->config->g_range * (float)raw_data_y / (float)(1 << (LSM6DS3_RESOLUTION - 1));
+	*z = sensor->config->g_range * (float)raw_data_z / (float)(1 << (LSM6DS3_RESOLUTION - 1));
+
+}
+
+
+void LSM6DS3_ReadGyro(LSM6DS3* sensor, float* x, float* y, float * z)
+{
+	/* get the rotation rate in degrees per second */
+}
+
+
+
+HAL_StatusTypeDef LSM6DS3_ReadRegister(LSM6DS3* sensor, uint8_t reg, uint8_t* data)
+{
+	uint8_t tx_buf[2] = {(reg | 0x80), 0x00};  // set the first bit to indicate a read communication
+	uint8_t rx_buf[2];
+
+	HAL_GPIO_WritePin(sensor->cs_port, sensor->cs_pin, GPIO_PIN_RESET);
+	HAL_StatusTypeDef status = HAL_SPI_TransmitReceive(sensor->spi, tx_buf, rx_buf, 2, HAL_MAX_DELAY);
+	HAL_GPIO_WritePin(sensor->cs_port, sensor->cs_pin, GPIO_PIN_SET);
+
+	*data = rx_buf[1];
+
+	return status;
+}
+
+
+HAL_StatusTypeDef LSM6DS3_WriteRegister(LSM6DS3* sensor, uint8_t reg, uint8_t data)
+{
+	uint8_t tx_buf[2] = {reg, data};
+
+	HAL_GPIO_WritePin(sensor->cs_port, sensor->cs_pin, GPIO_PIN_RESET);
+	HAL_StatusTypeDef status = HAL_SPI_Transmit(sensor->spi, tx_buf, 2, HAL_MAX_DELAY);
+	HAL_GPIO_WritePin(sensor->cs_port, sensor->cs_pin, GPIO_PIN_SET);
+
+	return status;
+}
+
+
+uint32_t LSM6DS3_GetInt1State(LSM6DS3* sensor)
+{
+	return GPIO_PIN_SET == HAL_GPIO_ReadPin(sensor->int1_port, sensor->int1_pin);
+}
+
+
+uint32_t LSM6DS3_GetInt2State(LSM6DS3* sensor)
+{
+	return GPIO_PIN_SET == HAL_GPIO_ReadPin(sensor->int2_port, sensor->int2_pin);
+}
